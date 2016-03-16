52c8ebefd3b7b5b7f9592d62d379cacea938b3d8
Daniel Barragán Calderón <d4n13lbc@users.noreply.github.com>
2016-03-15 20:54:16 -0500

    Update run_tests_with_coverage.sh

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -4,10 +4,10 @@
 
 rm -f pep8.log pyflakes.log
 
-PYTHONPATH=. python -m coverage run test.py
+PYTHONPATH=. coverage run test.py
 
-python -m coverage xml -o coverage.xml
-python -m coverage html -d coverage
+coverage xml -o coverage.xml
+coverage html -d coverage
 
 pep8 --max-line-length=120 pystache > pep8.log || true
 pyflakes pystache > pyflakes.log || true
